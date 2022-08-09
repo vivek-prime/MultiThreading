afd747ecba1736059d65382f665281be6a9dab68
vivek-prime <vivek.400428@gmail.com>
2022-08-09 23:54:56 +0530

    Executing tasks using Callable with multiple tasks

---
+++ src/main/java/MultipleCallableRunner.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class MultipleCallableRunner {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+//        create list of tasks
+        List<Callable<List<String>>> callableList = new ArrayList<>();
+        callableList.add(new Callable<List<String>>() {
+            @Override
+            public List<String> call() throws Exception {
+                return List.of("Vivek", "Choti");
+            }
+        });
+        callableList.add(new Callable<List<String>>() {
+            @Override
+            public List<String> call() throws Exception {
+                return List.of("Dhanbad", "Asansol");
+            }
+        });
+        List<Future<List<String>>> futures = executorService.invokeAll(callableList);
+        for (Future<List<String>> future : futures) {
+            System.out.println(future.get());
+        }
+        executorService.shutdown();
+    }
+}
